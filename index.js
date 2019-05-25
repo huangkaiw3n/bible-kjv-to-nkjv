07a9dbe5a3efb8db928ac6d9ff51ac6e7fd0744d
Huang Kaiwen <huangkaiw3n@gmail.com>
2019-05-25 13:10:08 +0800

    Refactor line reader

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const fs = require('fs')
 
 const nkjv = require('./nkjv.json').books
 
-const lr = new LineByLineReader('text.txt')
-
 let buffer = ''
 
-lr.on('error', function (err) {
-	// 'err' contains error object
-	console.log('error', err)
-})
+readFile('text.txt')
 
-lr.on('line', function (line) {
-	buffer = buffer + getNewLine(line) + '\n'
-})
+function readFile (filePath) {
+	const lr = new LineByLineReader(filePath)
 
-lr.on('end', function () {
-	console.log('ended reading text file.')
-	fs.writeFile('output.txt', buffer, (err) => {
-	    // throws an error, you could also catch it here
-	    if (err) throw err
-	    // success case, the file was saved
-	    console.log('File saved!')
+	lr.on('error', function (err) {
+		// 'err' contains error object
+		console.log('error', err)
 	})
-})
+
+	lr.on('line', function (line) {
+		buffer = buffer + getNewLine(line) + '\n'
+	})
+
+	lr.on('end', function () {
+		console.log('ended reading text file.')
+		fs.writeFile('output.txt', buffer, (err) => {
+		    // throws an error, you could also catch it here
+		    if (err) throw err
+		    // success case, the file was saved
+		    console.log('File saved!')
+		})
+	})
+}
 
 function getNewLine (line) {
 	let split = _.split(line, '\t')
